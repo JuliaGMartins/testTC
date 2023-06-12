321954c333b0655393575c6882c6861d27fafa1e
Julia Martins <julia.geremias.martins@gmail.com>
2023-06-11 22:41:20 -0300

    Projeto completo

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+function infoClick(x) {
+  if (x.getAttribute("src") === "assets/img/resultIcons/info.png") {
+    x.setAttribute("src", "assets/img/resultIcons/info-3.png");
+  } else {
+    x.setAttribute("src", "assets/img/resultIcons/info.png");
+  }
+}
+
+function presentationClick(x) {
+  if (x.getAttribute("src") === "assets/img/resultIcons/presentation.png") {
+    x.setAttribute("src", "assets/img/resultIcons/presentation-1.png");
+  } else {
+    x.setAttribute("src", "assets/img/resultIcons/presentation.png");
+  }
+}
+
+function resultsClick(x) {
+  if (x.getAttribute("src") === "assets/img/resultIcons/word.png") {
+    x.setAttribute("src", "assets/img/resultIcons/word-1.png");
+  } else {
+    x.setAttribute("src", "assets/img/resultIcons/word.png");
+  }
+}
+
+function tableClick(x) {
+  if (x.getAttribute("src") === "assets/img/resultIcons/table.png") {
+    x.setAttribute("src", "assets/img/resultIcons/table-1.png");
+  } else {
+    x.setAttribute("src", "assets/img/resultIcons/table.png");
+  }
+}
+
+function callClick(x) {
+  if (x.getAttribute("src") === "assets/img/resultIcons/call.png") {
+    x.setAttribute("src", "assets/img/resultIcons/call-1.png");
+  } else {
+    x.setAttribute("src", "assets/img/resultIcons/call.png");
+  }
+}
+
+function downloadClick(x) {
+  if (x.getAttribute("src") === "assets/img/downloadIcons/download.png") {
+    x.setAttribute("src", "assets/img/downloadIcons/download-1.png");
+  } else {
+    x.setAttribute("src", "assets/img/downloadIcons/download.png");
+  }
+}
